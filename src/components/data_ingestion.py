05fac514b4c6a901f4d3708bb0a47930e76abe89
Shiv Shankar S <shivshankarsajeev2000@gmail.com>
2025-05-31 07:56:36 +0530

    data_ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -22,6 +22,36 @@ class DataIngestion:
     def initaite_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            pass
-        except:
-            pass
+            df = pd.read_csv("Notebook\Data\hour_new.csv")
+            logging.info("Read the dataset as dataframe")
+
+            os.makedirs(
+                os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True
+            )
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(
+                self.ingestion_config.train_data_path, index=False, header=True
+            )
+
+            test_set.to_csv(
+                self.ingestion_config.test_data_path, index=False, header=True
+            )
+
+            logging.info("Ingestion of the data is completed")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+            )
+        except Exception as e:
+            raise CustomExcepiton(e, sys)
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initaite_data_ingestion()
